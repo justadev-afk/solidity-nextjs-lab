7585288cc334ed45ecfe7fa8faddcff33ae544c2
Julio Sansossio <juliosansossio@gmail.com>
2026-07-31 14:30:43 -0400

    contracts deploy

--- packages/abi/src/deployments.ts
+++ packages/abi/src/deployments.ts
@@ -10,7 +10,7 @@ export const deployments = {
     31337: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512",
   },
   Crowdfund: {
-    // 31337: "0x...",
+    31337: "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0",
   },
 } as const satisfies Record<string, Record<number, `0x${string}`>>;
 
